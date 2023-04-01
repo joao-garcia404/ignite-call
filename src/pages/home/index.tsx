276fd25c9506f2e6d2dae7c4fd10cc98af859fad
Joao Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-04-01 15:57:55 -0300

    Feat: Project config and Home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+
+import { Heading, Text } from '@ignite-ui/react'
+
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+import previewImage from '../../assets/app-preview.png'
+
+import { Container, Hero, Preview } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="App preview"
+        />
+      </Preview>
+    </Container>
+  )
+}
